f1fc0ebf3c5f58c02960ff56cbf0efc55b588c9e
Bruno <brunoseafightts@hotmail.com>
2023-12-06 08:35:18 -0300

    First commit

---
+++ src/routes/movieTags.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/movieTagsController");
+
+const movieTagsRoutes = Router();
+
+const tagsController = new TagsController()
+
+movieTagsRoutes.get("/:user_id", tagsController.index);
+
+module.exports = movieTagsRoutes;
